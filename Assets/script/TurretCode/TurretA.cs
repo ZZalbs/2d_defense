a4882eb8df1542130afa64e100d68bb467802e8e
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2022-11-07 01:21:46 +0900

    터렛 템플릿 제작

---
+++ Assets/script/TurretCode/TurretA.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretA : TurretInterface
+{
+    void Start()
+    {
+        StartCoroutine(TurretMain());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void TargetFind ()
+    {
+       
+    }
+
+    IEnumerator TurretMain()
+    {
+        TargetFind();
+        yield return new WaitForSeconds(this.attackDelay);
+    }
+}
